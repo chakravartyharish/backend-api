e46d61a3c2525caa798c464806e5ae43b9befa7e
Harish Kumar Chakravarty <103282023+chakravartyharish@users.noreply.github.com>
2024-08-14 15:57:57 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,36 @@
-const dotenv = require("dotenv")
-dotenv.config()
-const mongodb = require("mongodb")
+// const dotenv = require("dotenv")
+// dotenv.config()
+// const mongodb = require("mongodb")
 
-mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-  if (err) {
-    console.log(err)
+// mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+//   if (err) {
+//     console.log(err)
+//   }
+//   module.exports = client
+//   const app = require("./app")
+//   app.listen(process.env.PORT)
+// })
+
+const dotenv = require("dotenv");
+dotenv.config();
+const mongodb = require("mongodb");
+
+mongodb.connect(
+  process.env.CONNECTIONSTRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {
+    if (err) {
+      console.error("Failed to connect to the database. Error:", err);
+      process.exit(1); // Exit the process with an error code
+    }
+    console.log("Connected to MongoDB successfully");
+
+    module.exports = client;
+
+    const app = require("./app");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
   }
-  module.exports = client
-  const app = require("./app")
-  app.listen(process.env.PORT)
-})
+);
